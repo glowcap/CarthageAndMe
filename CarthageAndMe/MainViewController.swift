28bf9dceee92c4fec09609cf8b6f26bf3844b619
Daymein Gregorio <doku005@yahoo.com>
2018-02-05 15:22:30 +0900

    Initial Commit for Starter

---
+++ CarthageAndMe/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CarthageAndMe
+//
+//  Created by Daymein Gregorio on 2018/02/05.
+//  Copyright © 2018 Daymein Gregorio. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = #colorLiteral(red: 0.8705882353, green: 0.8509803922, blue: 0.8156862745, alpha: 1)
+  }
+
+}
+
